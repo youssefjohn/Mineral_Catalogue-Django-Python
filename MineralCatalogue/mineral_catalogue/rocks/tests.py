863bae938ef87b4c057a6dd97974a20c205d61a2
youssefjohn <youssefjohn89@gmail.com>
2019-04-24 01:59:45 +0100

    last update
    last upodate before I add docstrings.

--- MineralCatalogue/mineral_catalogue/rocks/tests.py
+++ MineralCatalogue/mineral_catalogue/rocks/tests.py
@@ -1,4 +1,5 @@
-from django.test import TestCase
+from django.urls import reverse
+from django.test import TestCase, Client
 from .models import Mineral
 from django.utils import timezone
 
@@ -76,48 +77,44 @@ class Test_Mineral_Model(TestCase):
 
 
         self.assertNotEqual(self.a,self.b)
-
+        self.assertIsInstance(self.a, Mineral)
+        self.assertIsInstance(self.b, Mineral)
 
 class Test_views(TestCase):
 
     def setUp(self):
-        self.a = Mineral.objects.create(name="daa",
-                                        image_filename="kasma",
-                                        image_caption="ansdkq",
-                                        category="kdCJXA",
-                                        formula="wDNA",
-                                        strunz_classification="KCDAX",
-                                        color="",
-                                        crystal_system="",
-                                        unit_cell="",
-                                        crystal_symmetry="",
-                                        cleavage="",
-                                        mohs_scale_hardness="",
-                                        luster="",
-                                        streak="",
-                                        diaphaneity="",
-                                        optical_properties="",
-                                        refractive_index="",
-                                        crystal_habit="",
-                                        specific_gravity="")
 
-        self.b = Mineral.objects.create(name="daa",
-                                        image_filename="kasma",
-                                        image_caption="ansdkq",
-                                        category="kdCJXA",
-                                        formula="wDNA",
-                                        strunz_classification="KCDAX",
-                                        color="",
-                                        crystal_system="",
-                                        unit_cell="",
-                                        crystal_symmetry="",
-                                        cleavage="",
-                                        mohs_scale_hardness="",
-                                        luster="",
-                                        streak="",
-                                        diaphaneity="",
-                                        optical_properties="",
-                                        refractive_index="",
-                                        crystal_habit="",
-                                        specific_gravity="")
+
+        self.client = Client()
+        self.details = reverse("rocks:details", args=[1]) # The details view requires a pk argument. Here I gave it the value of 1
+        self.random = reverse("rocks:random")
+        self.index = reverse("rocks:index")
+
+
+    def test_details_view(self):
+        response1 = self.client.get(self.details)
+        self.assertEqual(response1.status_code, 200)
+        self.assertTemplateUsed(response1, "rocks/details.html")
+
+    def test_random_view(self):
+        # resp = self.client.get(reverse("rocks:random"))
+        # self.assertEqual(resp.status_code,200)
+        # self.assertIn(self.a1, resp.context['random_mineral'])
+        # self.assertIn(self.a2, resp.context['random_mineral'])
+
+
+        response2 = self.client.get(self.random)
+        self.assertEqual(response2.status_code, 200)
+        self.assertTemplateUsed(response2, "rocks/random.html")
+
+
+
+
+    def test_index_view(self):
+        response3 = self.client.get(self.index)
+        self.assertEqual(response3.status_code, 200)
+        self.assertTemplateUsed(response3, "rocks/index.html")
+
+
+
 
